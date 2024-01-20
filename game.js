e652f7bbf5c5e425466069ab27019717608136d3
Marisa Saunders <marisaksaunders@gmail.com>
2024-01-19 20:22:33 -0500

    write conditionals for playRound()

--- game.js
+++ game.js
@@ -2,9 +2,28 @@ function getComputerChoice() {
   const weapon = ['rock', 'paper', 'scissor'];
   const random = weapon[(Math.floor(Math.random() * weapon.length))];
 
-  console.log(random)
+  return random
 
 }
 
-getComputerChoice()
+function playRound(playerSelection, computerSelection) {
+  const player
+  const player = playerSelection.toLowerCase()
+  const computer = computerSelection.toLowerCase()
+  if (player === 'rock' && computer === 'scissor') {
+    console.log('You Win! Rock beats Paper')
+  } else if (player === 'paper' && computer === 'rock') {
+    console.log('You Win! Paper beats Rock')
+  } else if (player === 'scissor' && computer === 'paper') {
+    console.log('You Win! Scissor beats paper')
+  } else if (player === 'rock' && computer === 'paper') {
+    console.log('You Lose! Paper beats rock') 
+  } else if (player === 'paper' && computer === 'scissor') {
+    console.log('You Lose! Scissor beats paper') 
+  } else if (player === 'scissor' && computer === 'rock') {
+    console.log('You Lose! Rock beats scissor')
+  } else if(player === computer) {
+    console.log(`It's a tie!`)
+  }
+}
 
