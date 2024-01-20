4dedad11e7142f338532675ad13665a14d3e8073
Marisa Saunders <marisaksaunders@gmail.com>
2024-01-19 22:26:55 -0500

    finish console game

--- game.js
+++ game.js
@@ -1,12 +1,19 @@
-const choices = ['rock', 'paper', 'scissors']
+const choices = ['rock', 'paper', 'scissors'];
+const winners = [];
 
 function game() {
-  playRound();
+  for(let i = 1; i <= 5; i++){
+    playRound(i);
+  }
+  logWins();
 }
 
-function playRound() {
+function playRound(round) {
   const playerSelection = playerChoice();
   const computerSelection = computerChoice();
+  const winner = checkWinner(playerSelection, computerSelection);
+  winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round);
 }
 
 function playerChoice() {
@@ -27,6 +34,7 @@ function playerChoice() {
     input = input.toLowerCase()
     check = validateInput(input);
   }
+  return input;
 }
 
 function computerChoice() {
@@ -34,10 +42,40 @@ function computerChoice() {
 }
 
 function validateInput(choice) {
-  return (choices.includes(choice));
+  return choices.includes(choice);
 }
 
-game();
+function checkWinner(choiceP, choiceC){
+  if(choiceP === choiceC) {
+    return 'Tie';
+  } else if (
+    (choiceP === "rock" && choiceC === 'scissors') || 
+    (choiceP === "paper" && choiceC === 'rock') || 
+    (choiceP === "scissors" && choiceC === 'paper')
+  ) {
+    return 'Player';
+  } else {
+    return 'Computer';
+  }
+}
+
+function logWins(){
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  let ties = winners.filter((item) => item == 'Tie').length;
+  console.log('Results:')
+  console.log('Player Wins:', playerWins);
+  console.log('Computer Wins:', computerWins);
+  console.log('Ties:', ties);
+}
+
+function logRound(playerChoice, computerChoice, winner, round){
+  console.log('Round:', round)
+  console.log('Player Chose:', playerChoice);
+  console.log('Computer Chose:', computerChoice);
+  console.log(winner, 'Won the Round');
+  console.log("---------------------------------")
+}
 
 
 
